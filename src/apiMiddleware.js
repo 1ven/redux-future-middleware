e102f2a61fbc50003f2e14e6e6df2f2b0b8b7462
1ven <1ven.mironchik@gmail.com>
2018-01-16 17:28:19 +0300

    Replace async by promise

--- src/apiMiddleware.js
+++ src/apiMiddleware.js
@@ -1,37 +1,40 @@
 import { symbol } from "./createApi";
 
-export default () => store => next => async action => {
+export default () => store => next => action => {
   if (!action[symbol]) {
     return next(action);
   }
 
-  try {
-    next({
-      type: action.types.request,
-      payload: action.payload
-    });
+  next({
+    type: action.types.request,
+    payload: action.payload
+  });
 
-    next({
-      type: action.types.success,
-      payload: {
-        request: action.payload,
-        body: await action.request(action.payload),
-        meta: {
-          receivedAt: Date.now()
+  action
+    .request(action.payload)
+    .then(body => {
+      next({
+        type: action.types.success,
+        payload: {
+          request: action.payload,
+          body,
+          meta: {
+            receivedAt: Date.now()
+          }
         }
-      }
-    });
-  } catch (err) {
-    next({
-      type: action.types.failure,
-      payload: {
-        request: action.payload,
-        message: err.message,
-        data: err.data,
-        meta: {
-          receivedAt: Date.now()
+      });
+    })
+    .catch(err => {
+      next({
+        type: action.types.failure,
+        payload: {
+          request: action.payload,
+          message: err.message,
+          data: err.data,
+          meta: {
+            receivedAt: Date.now()
+          }
         }
-      }
+      });
     });
-  }
 };
